cbb8f650bfb9dbc4519c267bfc279850713da30a
Daniel Maldonado <damarias991@gmail.com>
2024-02-28 06:25:28 -0500

    Range in for loops, sum_even_numbers.

---
+++ beginner/21.for_in_range.py
@@ -0,0 +1,7 @@
+# for number in range(0,10): -> goes from 0 to 9
+#    print(number) -> 0, 1, 2, ...,9
+
+total_sum = 0
+for number in range(0,1000):
+    total_sum += number 
+print(total_sum)
---
+++ beginner/22..sum_even_numbers.py
@@ -0,0 +1,14 @@
+# The program will calculate the sum of the even numbers from 1 to the use's
+# input number. The total sum will be printed.
+
+# User input the top number
+print('\nSUM OF THE EVEN NUMBERS\n')
+max_number = int(input('Enter the maximun number: '))
+
+# Iterate through numbers from 0 to max_number and sum the even ones
+even_sum = 0
+for number in range(0,max_number):
+    even_sum += number
+
+# Display the result
+print(f"The sum of even numbers from 0 to {max_number} is:\n{even_sum}")
