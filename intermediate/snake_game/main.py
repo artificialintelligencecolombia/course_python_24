0f58482ffe0629ec346140ee827cf0c15049b002
Daniel Maldonado <damarias991@gmail.com>
2024-06-05 21:41:19 -0500

    snake game: classes, movement and direction of the snake object

--- intermediate/snake_game/main.py
+++ intermediate/snake_game/main.py
@@ -1,5 +1,6 @@
 # Turtle is a python library for drawing shapes and patterns
 from turtle import Turtle, Screen
+from snake import Snake 
 import time
 import random
 
@@ -10,39 +11,24 @@ my_screen.bgcolor("black")
 my_screen.title("My snake game")
 my_screen.tracer(0) # Disables automatic screen updates. Give us manual control
 
-# Define the initial position of the turtles, in column coordinates
-init_positions_xaxis = [0, -20, -40] # Initial positions for the snake segments
-snake_body = []  # Create a list that composes the body of the snake in form of segments (turtle objects).
+# Create the snake
+snake = Snake()
 
-# Create the initial snake body with 3 segments
-for i in range(0, 3):  # Three turtle objects
-    new_turtle = Turtle()
-    new_turtle.shape("square") 
-    new_turtle.color("white") # Those are all the turtle's settings.
-    new_turtle.penup() # Prevent the turtle from drawing when moving
-    new_turtle.setposition(init_positions_xaxis[i], 0)
-    snake_body.append(new_turtle)   
+# Enable key event listening and binding arrow keys to the snake's move_ methods
+my_screen.listen()
+my_screen.onkey(snake.move_up, "Up")
+my_screen.onkey(snake.move_down, "Down") 
+my_screen.onkey(snake.move_left, "Left") 
+my_screen.onkey(snake.move_right, "Right") 
 
 # Create the token for the snake movement
 game_is_on = True
-
-directions = [0, 90, 180, 270]
-key_list = ['a','w','s','d','c']
-
 # Main game loop
 while game_is_on: 
     init_speed = 1
     my_screen.update() # Update the screen manually
     time.sleep(0.1) # Pause the execution for 0.1 seconds to control the game speed
-    my_screen.listen()
-    
-    
-    for segment in range(len(snake_body)-1, 0, -1):
-        new_x = snake_body[segment-1].xcor()
-        new_y = snake_body[segment-1].ycor()
-        snake_body[segment].goto(new_x,new_y)
-
-    snake_body[0].forward(20)
-    snake_body[0].right(90)    
+    snake.move() # Move the snake
+     
 # Close the window on click
 my_screen.exitonclick()
---
+++ intermediate/snake_game/snake.py
@@ -0,0 +1,49 @@
+# Create the class snake for the game.
+
+# Import required packages/modules
+from turtle import Turtle
+
+# Create the constants used in the object class
+START_POSITIONS_XAXIS = [0, -20, -40] # Initial positions for the snake segments
+DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake():
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake() # Create the snake once the object is instantiated
+        
+    def create_snake(self):
+        # Create the initial snake body with 3 segments
+        for i in range(0, 3):  # Three turtle objects
+            new_turtle = Turtle()
+            new_turtle.shape("square") 
+            new_turtle.color("white") # Those are all the turtle's settings.
+            new_turtle.penup() # Prevent the turtle from drawing when moving
+            new_turtle.setposition(START_POSITIONS_XAXIS[i], 0)
+            self.snake_body.append(new_turtle)   
+            
+    def move(self):
+        # Iterate over the snake body segments from the last segment (tail) to the second segment. 
+        for segment in range(len(self.snake_body)-1, 0, -1): # From the last segment to the first segment (excluding the head)
+            # Get the x,y-coordinates of the previous segment
+            new_x = self.snake_body[segment-1].xcor() 
+            new_y = self.snake_body[segment-1].ycor()
+            # Move the current segment to the position of the previous segment
+            self.snake_body[segment].goto(new_x,new_y) 
+        self.snake_body[0].forward(20)
+        
+    def move_up(self):
+        self.snake_body[0].setheading(UP)
+        
+    def move_down(self):
+        self.snake_body[0].setheading(DOWN)
+    
+    def move_left(self):
+        self.snake_body[0].setheading(LEFT)
+    
+    def move_right(self):
+        self.snake_body[0].setheading(RIGHT)   
