20590c825adb51958e84dcd5d9b5921aef0a0fe5
Daniel Maldonado <damarias991@gmail.com>
2024-06-19 17:19:12 -0500

    pong game: ball class and movements

---
+++ intermediate/pong_game/ball.py
@@ -0,0 +1,35 @@
+# Create the ball class that renders itself as a small cirlce (form).
+
+from turtle import Turtle
+
+# Ball class is going to inherit attrs and methods from the Turtle class
+class Ball(Turtle):
+    def __init__(self):
+        # Call the constructor of the parent class (Turtle)
+        super().__init__()
+        self.speed(1)  # Set the speed of animation
+        self.shape("circle") # Sets the shape of the Ball object to a circle. Using 'self' to set properties on the current instance.
+        self.color("white")
+        self.penup()  # Make sure the ball doesn't draw when it moves
+        self.goto(0,0)
+        self.x_move = 10  # Initial movement in the x-direction
+        self.y_move = -10  # Initial movement in the y-direction
+    
+    def move(self):
+        # Calculate the new position
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)  # Move the ball to the new position
+        
+    def wall_bounce(self):
+        # Reverse the y-direction
+        self.y_move *= -1
+        
+    def bounce_x(self):
+        # Reverse the y-direction
+        self.x_move *= -1    
+    
+    def reset_position(self):
+        # Move the ball back to the center and reverse the x-direction
+        self.goto(0, 0)
+        self.bounce_x()
--- intermediate/pong_game/main.py
+++ intermediate/pong_game/main.py
@@ -1,6 +1,7 @@
 # Import required library
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
 
 # Constants
 USR_PADDLE_POSITION_X = -380
@@ -10,20 +11,32 @@ screen = Screen()
 screen.title("Pong Game")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
-# window.tracer(0)  # Stops the window from updating automatically
+# screen.tracer(0)  # Stops the window from updating automatically
 
-# Create the paddle 
+# Create the paddles & ball 
 usr_paddle = Paddle((USR_PADDLE_POSITION_X,0)) # Left paddle at (-380, 0)
 second_paddle = Paddle((-USR_PADDLE_POSITION_X,0)) # Right paddle at (380, 0)
+ball = Ball()
 
 # Enable key event listening 
 screen.listen()
 
 # Binding arrow keys to the usr paddle's move_ methods
-screen.onkey(usr_paddle.move_up, "Up")
-screen.onkey(usr_paddle.move_down, "Down") 
-screen.onkey(second_paddle.move_up, "o")
-screen.onkey(second_paddle.move_down, "l") 
+screen.onkey(usr_paddle.move_up, "w")
+screen.onkey(usr_paddle.move_down, "s") 
+screen.onkey(second_paddle.move_up, "Up")
+screen.onkey(second_paddle.move_down, "Down") 
+
+# Create the token for the snake movement
+game_is_on = True
+# Main game loop
+while game_is_on:
+    # screen.update()  # Manually update the screen 
+    ball.move()
+    
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.wall_bounce()
 
 # Close the window on click
 screen.exitonclick()
