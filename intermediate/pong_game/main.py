53f3c96068d305d12078cc8486d68c34d5090648
Daniel Maldonado <damarias991@gmail.com>
2024-06-19 15:54:06 -0500

    pong game: create and move the second paddle

--- intermediate/pong_game/main.py
+++ intermediate/pong_game/main.py
@@ -2,15 +2,28 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 
+# Constants
+USR_PADDLE_POSITION_X = -380
+
 # Set up the screen
-window = Screen()
-window.title("Pong Game")
-window.bgcolor("black")
-window.setup(width=800, height=600)
+screen = Screen()
+screen.title("Pong Game")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
 # window.tracer(0)  # Stops the window from updating automatically
 
-# Create the snake, food and score
-right_paddle = Paddle()
+# Create the paddle 
+usr_paddle = Paddle((USR_PADDLE_POSITION_X,0)) # Left paddle at (-380, 0)
+second_paddle = Paddle((-USR_PADDLE_POSITION_X,0)) # Right paddle at (380, 0)
+
+# Enable key event listening 
+screen.listen()
+
+# Binding arrow keys to the usr paddle's move_ methods
+screen.onkey(usr_paddle.move_up, "Up")
+screen.onkey(usr_paddle.move_down, "Down") 
+screen.onkey(second_paddle.move_up, "o")
+screen.onkey(second_paddle.move_down, "l") 
 
 # Close the window on click
-window.exitonclick()
+screen.exitonclick()
--- intermediate/pong_game/paddle.py
+++ intermediate/pong_game/paddle.py
@@ -3,19 +3,28 @@
 # Import required packages/modules
 from turtle import Turtle
 
-# Create the constants used in the object class
-USR_PADDLE_POSITION_X = 380
-
 class Paddle():
-    def __init__(self):
-        self.create_paddle()
+    def __init__(self, position): # constructor method. Initialize the paddle at a specified position
+        self.create_paddle(position)
+      
+    def create_paddle(self, position):
+        self.paddle = Turtle() # Create a turtle object for the paddle
+        self.paddle.speed(0) # Set the speed to the maximum (no animation delay)
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=3, stretch_len=2)
+        self.paddle.penup() # Set the speed to the maximum (no animation delay)
+        self.paddle.goto(position) # Move the paddle to the specified position
+    
+    # Functions to move the paddle vertically    
+    def move_up(self):
+        # Move the paddle up by 20 pixels
+        y = self.paddle.ycor() # Get current y-coordinate
+        y += 20 # Increase y-coordinate
+        self.paddle.sety(y) # Set new y-coordinate
         
-    def create_paddle(self):
-        # Create the paddle
-        paddle = Turtle()
-        paddle.speed(0)
-        paddle.shape("square")
-        paddle.color("white")
-        paddle.shapesize(stretch_wid=3, stretch_len=2)
-        paddle.penup()
-        paddle.goto(USR_PADDLE_POSITION_X, 0)
+    def move_down(self):
+        # Move the paddle down by 20 pixels
+        y = self.paddle.ycor()  # Get current y-coordinate
+        y -= 20  # Decrease y-coordinate
+        self.paddle.sety(y)  # Set new y-coordinate
