cd4b9e07b98b0e7543c290b728c73ae633b3afb9
Daniel Maldonado <damarias991@gmail.com>
2024-07-08 18:06:12 -0500

    read and  write text files.

---
+++ intermediateii/read_write_files.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+
+# Open the file in read and append mode. It closes it automatically
+with open('my_text_file.txt', 'r+') as f: 
+    # Read the file -> by using read() method
+    content = f.read()
+    
+    # Print content before append
+    print('Content before append: ')
+    print(content)
+    
+    new_text = '\nbut I love elephants.'
+    
+    # Write on the file
+    f.write(new_text)
+    
+    # Move the file pointer to the beginning of the file before reading the updated content
+    f.seek(0)
+    
+    # Print the content of the file after append the new text
+    new_content = f.read()
+    print('Content after append: ')
+    print(new_content)
