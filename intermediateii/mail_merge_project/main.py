2837462e418b0bf4a4e80197cae565809a22a7b2
Daniel Maldonado <damarias991@gmail.com>
2024-07-10 19:23:03 -0500

    multiple mail project: COMPLETED

--- intermediateii/mail_merge_project/main.py
+++ intermediateii/mail_merge_project/main.py
@@ -1,38 +1,52 @@
 #!usr/bin/env python3
 
+import os # Handles file paths and dir operations.
+
 # Set the filepaths and the modes
 blueprint_filepath = './Input/Letters/starting_letter.txt'
-blpnt_mode = 'r+'
 list_of_invitees_filepath = './Input/Names/invited_names.txt'
+blpnt_mode = 'r+'
 invt_mode = 'r'
 list_of_names = []
+output_dir = './Output/ReadyToSend' # Where personalized messages will be saved.
 
-# Created a fn that reads the files and returns the content -> str.
+# Fn that reads the files and returns the content -> str.
 def read_file(filename, mode):
-    with open(filename, mode) as file:
-        # If blueprint file, we need to edit it.
-        if mode == 'r+':
-            content = file.read()
-            return content
-        # If the file is the name list, only read.
-        if mode == 'r':
-            lines = file.readlines()
-            return [line.strip() for line in lines]       
-
+    try:
+        with open(filename, mode) as file:
+            # If blueprint file, we need to edit it.
+            if mode == 'r+':
+                content = file.read()
+                return content
+            # If the file is the name list, only read.
+            if mode == 'r':
+                lines = file.readlines()
+                return [line.strip() for line in lines]       
+    except FileNotFoundError:
+        print(f"Error: the file {filename} was not found.")
+        return None
+    except Exception as e:
+        print(f"Error occurred while reading {filename}: {e}")
+        return 
+    
 # store the elements in variables
 file = read_file(blueprint_filepath, blpnt_mode)
 list = read_file(list_of_invitees_filepath, invt_mode)
 
 # Set the counter of the letters 
-tk = 0
+tk = 1
+
+# Loop for creating the list of personalized invitations
 for name in list:
     invitation_letter = file.replace('[name]', name)
-    print(f"INVITEE N.{tk}")
-    print(invitation_letter)
-    
+    # print(f"INVITEE N.{tk}")
+    # print(invitation_letter)
+    new_name = f'invitation_{name}.txt'
+    filepath = os.path.join(output_dir, new_name) 
+    with open(filepath, 'w') as f:
+        f.write(invitation_letter)
+    print(f"Invitation number {tk} for {name}: CREATED.")
     tk += 1
-
-
 
 
 #TODO: Create a letter using starting_letter.txt 
