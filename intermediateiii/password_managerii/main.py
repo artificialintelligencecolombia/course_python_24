cf77c0e81dae4e5cd6dc8b36f89ec6d27354228d
artificialintelligencecolombia <damarias991@gmail.com>
2024-08-29 06:51:21 -0500

    password generator2.0 gui

--- intermediateiii/password_managerii/main.py
+++ intermediateiii/password_managerii/main.py
@@ -21,33 +21,33 @@ canvas = Canvas(width=200, height=200)
 img_path = os.path.join(os.path.dirname(__file__), "logo.png") # Get the directory path of the current Python script file -> Concatenate it with the image file name "logo.png" to construct the full absolute path to the image file.
 lock_img = PhotoImage(file=img_path) # Loads the image from a file
 canvas.create_image(100,100, image=lock_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
 # Labels
 website = Label(text="Website:")
-website.pack()
+website.grid(row=1, column=0)
 
 email = Label(text="Email:")
-email.pack()
+email.grid(row=2, column=0)
 
 passwd = Label(text="Password: ")
-passwd.pack()
+passwd.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry()
-website_entry.pack()
+website_entry.grid(row=1, column=1, columnspan=2)
 
 email_entry = Entry()
-email_entry.pack()
+email_entry.grid(row=2, column=1, columnspan=2)
 
 passwd_entry = Entry()
-passwd_entry.pack()
+passwd_entry.grid(row=3, column=1)
 
 # Buttons
 add_btn = Button(text="Add")
-add_btn.pack()
+add_btn.grid(row=4, column=1, columnspan=2)
 
 genpass_btn = Button(text="Generate Password")
-genpass_btn.pack()
+genpass_btn.grid(row=3, column=2)
 
 window.mainloop()
