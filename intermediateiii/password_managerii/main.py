6e7ed6a1db067aff4b048149a5628d690b4df832
artificialintelligencecolombia <damarias991@gmail.com>
2024-08-30 11:34:39 -0500

    password manager: storing passwds in .txt file

--- intermediateiii/password_managerii/main.py
+++ intermediateiii/password_managerii/main.py
@@ -3,14 +3,21 @@ from tkinter import *
 import os
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# ---------------------------- 2. SAVE PASSWORD ------------------------------- #
 
+# Get the values of the entries
+def test():
+    website_str = website_entry.get()
+    email_str = email_entry.get()
+    psswd_str = passwd_entry.get()
+
+    with open("./intermediateiii/password_managerii/pswd_manager.txt", "a") as f:
+        f.write(f"{website_str} | {email_str} | {psswd_str}")
 
 # ---------------------------- 1.UI SETUP ------------------------------- #
 # Window
 window = Tk()
 window.title("Password Generator 2.0")
-window.minsize(width=500, height=300)
 window.config(padx=20, pady=20)
 
 
@@ -21,7 +28,7 @@ canvas = Canvas(width=200, height=200)
 img_path = os.path.join(os.path.dirname(__file__), "logo.png") # Get the directory path of the current Python script file -> Concatenate it with the image file name "logo.png" to construct the full absolute path to the image file.
 lock_img = PhotoImage(file=img_path) # Loads the image from a file
 canvas.create_image(100,100, image=lock_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1, columnspan=2)
 
 # Labels
 website = Label(text="Website:")
@@ -44,7 +51,7 @@ passwd_entry = Entry(width=21)
 passwd_entry.grid(row=3, column=1)
 
 # Buttons
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=test)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 genpass_btn = Button(text="Generate Password")
